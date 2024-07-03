a83b76a69186fb439741a4bdf5b0fa26eda4b3a3
FilipeSantosSoares <filipedossantossoares9@gmail.com>
2024-07-03 18:58:08 -0300

    first commit

---
+++ javascript/menu.js
@@ -0,0 +1,13 @@
+function menu() {
+  const menuMobile = document.getElementById("menu-mobile");
+
+  menuMobile.addEventListener("click", function () {
+      const menuMobileOptions = document.getElementById("menu-mobile-options");
+      menuMobileOptions.classList.toggle("active-menu");
+      menuMobile.classList.toggle("animationTransform");
+      document.body.classList.toggle("overflow-body");
+  });
+}
+
+document.addEventListener("DOMContentLoaded", menu);
+  
